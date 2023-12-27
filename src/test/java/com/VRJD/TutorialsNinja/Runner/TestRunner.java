6cb8f83900dea9d63c07740c89188dc66973e39a
Varadaraj <varadaraj.vv8@gmail.com>
2023-12-27 12:21:00 +0530

    Modified Test Runner File

--- src/test/java/com/VRJD/TutorialsNinja/Runner/TestRunner.java
+++ src/test/java/com/VRJD/TutorialsNinja/Runner/TestRunner.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
         publish=true,
         plugin = { "pretty","html:test-output/HTMLReport.html", "json:test-output/JSONReport.json",
                    "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:", }, 
-                	tags = "@Regression", dryRun = false, monochrome = true)
+                	tags = "@Sanity", dryRun = false, monochrome = true)
 
 public class TestRunner {
 
